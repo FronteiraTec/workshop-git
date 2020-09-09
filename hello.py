c73428798b7904036eff63ec10b5f86a4ea70583
fersasil <gfersasil@gmail.com>
2020-09-09 17:13:32 -0300

    arquivo de exemplo adicionado

---
+++ hello.py
@@ -0,0 +1,22 @@
+# As vezes, quando você programa,
+# Você se engana 
+# Mas o git não te abandona! 
+
+# Apague estes comentários,
+# E print o teu nome!
+# print('FronteiraTec')
+
+
+# Apague esses comentários e linhas brancas também!
+# O que você deve fazer?
+# Criar uma nova branch! 
+# Como eu crio uma nova branch?
+# Sera que é com a mente?
+# Nem mesmo tente!
+# Siga com a gente!
+# git checkout -b "suaNovaBranch"
+# git add . 
+# git commit -m "suaMudança!"
+# git push
+# e a sua senha vamos precisar
+# Depois vua á lá! Lá vai estar!
