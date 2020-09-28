40bcd9eab10938065db88b5c3b2e3bd6521185de
fersasil <gfersasil@gmail.com>
2020-09-28 03:51:48 -0300

    Exemplo de teste

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php 
-echo $_SERVER['REQUEST_URI'];
-header( 'Location: ' . $_SERVER['REQUEST_URI']) ; 
+// echo $_SERVER['REQUEST_URI'];
+
+$url = $_SERVER['REQUEST_URI'] == '/' ? './index.html' : '.' . $_SERVER['REQUEST_URI'];
+// echo $url;
+// header( 'Location: ' . $url) ; 
+$html = require($url);
+echo $html;
  ?>
